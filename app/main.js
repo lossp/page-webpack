445198c38094ab519c77e311af26f8ef78060cd6
lossp <richard.xxmxx@gmail.com>
2017-08-07 13:12:19 +0800

    webpack

---
+++ app/main.js
@@ -0,0 +1,8 @@
+var $ = require('./jquery.min.js');
+
+var carousel = require('./Carousel.js');
+var gotop = require('./gotop.js');
+var waterfall = require('./waterfall.js');
+new carousel();
+new waterfall();
+new gotop();
